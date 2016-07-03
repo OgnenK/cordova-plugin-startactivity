cced85082619005199dc8f8e2aed6ede5f85e8f1
OgnenK <ognen.kostovski@gmail.com>
2016-07-03 16:24:30 +0200

    Update StartActivity.js

--- www/StartActivity.js
+++ www/StartActivity.js
@@ -14,27 +14,27 @@ StartActivity.launchApp = function(appName, success, error) {
 	exec(success, error, 'StartActivity', 'launchApp', [appName]);
 
 
-}
+};
 StartActivity.launchSpecificApp = function(appName, success, error) {
 
 	exec(success, error, "StartActivity", 'launchSpecificApp', [appName]);
 
 
-}
+};
 StartActivity.launchAppForResults : function(appName, success, error) {
 
 
     	exec(success, error, 'StartActivity', 'launchAppForResults', [appName]);
 
 
-}
+};
 StartActivity.recieveResults : function(appName, success, error) {
 
 
     	exec(success, error, 'StartActivity', 'recieveResults', []);
 
 
-}
+};
 
 
 
